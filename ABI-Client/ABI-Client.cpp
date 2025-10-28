30ad8e9864f4001e69bbc140542d12c6948b9300
BuildTools <unconfigured@null.spigotmc.org>
2025-10-28 22:40:54 +0100

    CLient mit KeepAlive und Per Request

--- ABI-Client/ABI-Client.cpp
+++ ABI-Client/ABI-Client.cpp
@@ -14,15 +14,78 @@ int main()
 	system("cls");
 	system("color 4B");   // rot/blau Fensterfarbe
 	system("mode 80,25"); // Konsole breiter machen
-
-	cout << "=== TCP-Client ===" << endl;
-	cout << "Verbinde mit Server " << SERVER_IP << ":" << SERVER_PORT << " ..." << endl;
-	while (1) {
-	// 1) Socket-Objekt fuer den Client erzeugen
-	Socket client(SERVER_IP, SERVER_PORT);
-
-	// 2) Verbindung zum Server aufbauen (mehrfach versuchen)
 	
+	cout << "[1] Per-Request\n"
+		<< "[2] Keep-Alive\n\n"
+		<< ">";
+	string ans;
+	getline(cin, ans);
+
+	if (ans == "1") {
+		while (1) {
+			system("cls");
+			system("color 4B");   // rot/blau Fensterfarbe
+			system("mode 80,25"); // Konsole breiter machen
+
+			cout << "=== TCP-Client ===" << endl;
+			cout << "Verbinde mit Server " << SERVER_IP << ":" << SERVER_PORT << " ..." << endl;
+			// 1) Socket-Objekt fuer den Client erzeugen
+			Socket client(SERVER_IP, SERVER_PORT);
+
+			// 2) Verbindung zum Server aufbauen (mehrfach versuchen)
+
+			while (client.connect() == 0)
+			{
+				cout << ".";
+				Sleep(400); // 400ms warten, Server evtl. noch nicht gestartet
+			}
+
+			cout << "\nVerbindung erfolgreich aufgebaut!" << endl;
+
+			// 3) Bedienungsanleitung fuer den Nutzer
+			cout << endl
+				<< "Verfuegbare Befehle:" << endl
+				<< " TIME               -> Server-Uhrzeit abfragen" << endl
+				<< " ECHO <Text>        -> Text wird zurueckgesendet" << endl
+				<< " ADD <a> <b>        -> Server addiert die Zahlen" << endl
+				<< " EXIT               -> Verbindung beenden" << endl;
+
+			// 4) Eingabe-Schleife: Benutzer schickt Anfragen an den Server
+			string befehl = "";
+
+			cout << "\n> ";
+			std::getline(cin, befehl); // ganze Zeile einlesen
+
+			// Anfrage an den Server schicken 
+			client.write(befehl);
+
+			// Wenn EXIT: Verbindung direkt beenden
+			if (befehl == "EXIT")
+			{
+				cout << "Verbindung wird beendet..." << endl;
+				client.close();
+				return 0;
+			}
+
+			// 5) Antwort vom Server lesen (bis '\n')
+			string antwort = client.readLine();
+			cout << "< " << antwort << endl;
+
+			// 6) Verbindung sauber beenden
+			client.close();
+
+			cout << "Client wurde beendet." << endl;
+			system("pause");
+		}
+	}
+	else if(ans == "2") {
+
+
+		// 1) Socket-Objekt fuer den Client erzeugen
+		Socket client(SERVER_IP, SERVER_PORT);
+
+		// 2) Verbindung zum Server aufbauen (mehrfach versuchen)
+
 		while (client.connect() == 0)
 		{
 			cout << ".";
@@ -30,41 +93,45 @@ int main()
 		}
 
 		cout << "\nVerbindung erfolgreich aufgebaut!" << endl;
+		while (1) {
+			system("cls");
+			system("color 4B");   // rot/blau Fensterfarbe
+			system("mode 80,25"); // Konsole breiter machen
 
-		// 3) Bedienungsanleitung fuer den Nutzer
-		cout << endl
-			<< "Verfuegbare Befehle:" << endl
-			<< " TIME               -> Server-Uhrzeit abfragen" << endl
-			<< " ECHO <Text>        -> Text wird zurueckgesendet" << endl
-			<< " ADD <a> <b>        -> Server addiert die Zahlen" << endl
-			<< " EXIT               -> Verbindung beenden" << endl;
+			cout << "=== TCP-Client ===" << endl;
+			cout << "Verbinde mit Server " << SERVER_IP << ":" << SERVER_PORT << " ..." << endl;
+			// 3) Bedienungsanleitung fuer den Nutzer
+			cout << endl
+				<< "Verfuegbare Befehle:" << endl
+				<< " TIME               -> Server-Uhrzeit abfragen" << endl
+				<< " ECHO <Text>        -> Text wird zurueckgesendet" << endl
+				<< " ADD <a> <b>        -> Server addiert die Zahlen" << endl
+				<< " EXIT               -> Verbindung beenden" << endl;
 
-		// 4) Eingabe-Schleife: Benutzer schickt Anfragen an den Server
-		string befehl = "";
+			// 4) Eingabe-Schleife: Benutzer schickt Anfragen an den Server
+			string befehl = "";
 
-		cout << "\n> ";
-		std::getline(cin, befehl); // ganze Zeile einlesen
+			cout << "\n> ";
+			std::getline(cin, befehl); // ganze Zeile einlesen
 
-		// Anfrage an den Server schicken 
-		client.write(befehl);
+			// Anfrage an den Server schicken 
+			client.write(befehl);
 
-		// Wenn EXIT: Verbindung direkt beenden
-		if (befehl == "EXIT")
-		{
-			cout << "Verbindung wird beendet..." << endl;
-			client.close();
-			return 0;
+			// Wenn EXIT: Verbindung direkt beenden
+			if (befehl == "EXIT")
+			{
+				cout << "Verbindung wird beendet..." << endl;
+				cout << "Client wurde beendet." << endl;
+				system("pause");
+				client.close();
+				return 0;
+			}
+
+			// 5) Antwort vom Server lesen (bis '\n')
+			string antwort = client.readLine();
+			cout << "< " << antwort << endl;
+			system("pause");
 		}
-
-		// 5) Antwort vom Server lesen (bis '\n')
-		string antwort = client.readLine();
-		cout << "< " << antwort << endl;
-
-		// 6) Verbindung sauber beenden
-		client.close();
-
-		cout << "Client wurde beendet." << endl;
-		system("pause");
 	}
 	return 0;
 }
